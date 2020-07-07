b88b80cd233fd0bff6be72ee879a66873e043579
Thomas Dell <>
2020-07-07 10:56:34 +0100

    Create Header and counter button

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,7 @@ import './App.css'
 import Scroll from '../components/Scroll'
 import ErrorBoundry from '../components/ErrorBoundry'
 import { setSearchField, requestRobots } from '../actions'
+import Header from '../components/Header'
 
 
 const mapStateToProps = state => {
@@ -42,7 +43,7 @@ export class App extends Component {
         else {
             return (
                 <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
+                    <Header/>
                     <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
