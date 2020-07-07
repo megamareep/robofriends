289b1b25ca6915c2037e9261fb115a72f735d034
megamareep <megamareep@gmail.com>
2020-07-07 11:33:43 +0100

    Turn off logger middleware

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
-import { createLogger } from 'redux-logger'
+//import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import './index.css';
 import App from './containers/App'
@@ -10,13 +10,13 @@ import * as serviceWorker from './serviceWorker';
 import { searchRobots, requestRobots } from './reducers'
 import 'tachyons'
 
-const logger = createLogger();
+//const logger = createLogger();
 //applyMiddleware ordered
 const rootReducer = combineReducers({searchRobots, requestRobots});
 //thunk middleware looking for function returns. So it can listen for callbacks.
 //store one single object that holds state for all components.
 //components no longer need to hold state.
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
   <React.StrictMode>
